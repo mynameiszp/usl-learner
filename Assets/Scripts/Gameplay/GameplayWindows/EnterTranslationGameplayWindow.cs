553374c297420dd10a85f29aaa5a240eb532df44
Anastasiia Zaporozhchenko <nastyazaporozhchenko@gmail.com>
2025-04-28 19:12:32 +0300

    Added enter window

---
+++ Assets/Scripts/Gameplay/GameplayWindows/EnterTranslationGameplayWindow.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class EnterTranslationGameplayWindow : BasicGameplayWindow
+{
+    [SerializeField] private TMP_InputField inputText;
+    [SerializeField] private Button enterButton;
+    [SerializeField] private TMP_Text failText;
+
+    public override void SetVisual()
+    {
+        handAnimation.AnimationState.SetAnimation(0, word, true);
+        handAnimation.gameObject.SetActive(true);
+        failText.gameObject.SetActive(false);
+        continueBut.gameObject.SetActive(false);
+
+        enterButton.onClick.AddListener(CheckAnswer);
+    }
+
+    private void CheckAnswer(){
+        if (inputText.text.Trim() == word) //check for synonyms??
+        {
+            failText.gameObject.SetActive(false);
+            continueBut.gameObject.SetActive(true);
+            enterButton.enabled = false;
+        }
+        else
+        {
+            failText.gameObject.SetActive(true);
+        }
+    }
+}
